74f6c9bc6569e242e85abdb3998157b520c121ad
yashi <yasuhito.toyama@yashilab.com>
2023-04-20 23:04:24 +0900

    Revert "Chakra UI を追加"

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,6 @@
-import { ChakraProvider } from '@chakra-ui/react'
+import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 
-const Portfolio = ({ Component, pageProps }: AppProps) => {
-  return (
-    <ChakraProvider>
-      <Component {...pageProps} />
-    </ChakraProvider>
-  )
+export default function App({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />
 }
-
-export default Portfolio
